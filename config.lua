0454ef53c40bc4be9ed55d786d072e469fbd34b2
pleleu-dev <pleleu.dev@gmail.com>
2024-05-20 23:30:57 +0200

    Improve Neotree config - add git icons

--- config.lua
+++ config.lua
@@ -110,6 +110,11 @@ end
 
 
 
+--
+--
+-- START NEO TREE CONFIGURATION
+--
+--
 local neo_tree_cgf = {
   close_if_last_window = true,
   window = {
@@ -132,12 +137,30 @@ local neo_tree_cgf = {
       },
     },
   },
+  default_component_configs = {
+    git_status = {
+      symbols = {
+        -- Change type
+        added     = "✚", -- NOTE: you can set any of these to an empty string to not show them
+        deleted   = "✖",
+        modified  = "",
+        renamed   = "󰁕",
+        -- Status type
+        untracked = "",
+        ignored   = "",
+        unstaged  = "󰄱",
+        staged    = "",
+        conflict  = "",
+      },
+      align = "right",
+    },
+  },
 }
-
-
-
-
-
+--
+--
+-- END NEO TREE CONFIGURATION
+--
+--
 -- Formatter
 lvim.format_on_save.enabled = true
 
